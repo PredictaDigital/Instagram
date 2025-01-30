3bc09905048bbcee573b6d2c7a4580a9f0b6dafe
PredictaDigital <analytics@predictadigital.com>
2025-01-30 15:51:35 +0530

    Update Instagram_Page_Statistics_Lifetime.py

--- Instagram_Page_Statistics_Lifetime.py
+++ Instagram_Page_Statistics_Lifetime.py
@@ -2,34 +2,34 @@ import requests
 import pyodbc
 
 # Facebook API endpoint
-API_VERSION = 'v19.0'  # Specify the desired Graph API version
+API_VERSION = 'v22.0'  # Specify the desired Graph API version
 PAGE_ID = '17841448939781055'
-ACCESS_TOKEN = 'EAAFe1V0tVGYBO74mh9jnZC6OI7UD5UuB5jmCJfibkZBFOnX9dNwHTbW6ClsHGJHSrY5gKBAun7nsfUMvImj0mVjUwmQBK466CIKa3ut2hhqa8d1BnUV8aOcxHbxyWoMEJrDUWCb5an9KdmEriOPob6DTDpZBJEhY31tJlhpyhBEaSmjpROJCZBZBNrKAmcxd8uX9ZAynOQvdBLhiUV5mo2PaiZAmOG4UjRZCfsZCjD2X6'
-endpoint = fhttpsgraph.facebook.com{API_VERSION}{PAGE_ID}
+ACCESS_TOKEN = 'EAAFe1V0tVGYBO9K7VJLBPIUVqZCS1OjtjJQZBdw7miSqRHedaifZB6F4dCgPY6FXHfF4hRqXaq2JOUeZBzxsyx2bEBn8b3mWjUjezzrL6CJPL5IlLJgjEZCp1zFq1ZAVWlXDIAmZAPZA9v8NZCJKNC80ZBpCnC2Mg8BL5GL8dTWZCDIDJOHoiC434cafURV7HPcInPIUpb3ZBkw1tZBk0a3So9fBz7DC2oBRpKiFdirWUd4YZD'
+endpoint = f'https://graph.facebook.com/{API_VERSION}/{PAGE_ID}'
 # Define the parameters for the request 17841448939781055
 params = {
-    'access_token' ACCESS_TOKEN,
-    'fields' 'id, name,username,followers_count,follows_count,media_count,website,profile_picture_url,biography'
+    'access_token': ACCESS_TOKEN,
+    'fields' :'id, name,username,followers_count,follows_count,media_count,website,profile_picture_url,biography'
 }
 response = requests.get(endpoint, params=params)
 
 analytics_data = response.json()
 # print(analytics_data)
-
 #---------------------------------------------------------------------------------------------
 # Establish a connection to your SQL Server database
 server = 'Predicta.Database.Windows.Net'
 database = 'Predicta'
 username = 'PredictaAdmin'
-password = 'Yhf^43&^FHHytf'
+password = 'Yhf^43*&^FHHytf'
+db_table = 'dbo.Instagram_Page_Statistics_Lifetime_KS'
 
 conn = pyodbc.connect(f'DRIVER={{SQL Server}};SERVER={server};DATABASE={database};UID={username};PWD={password}')
 cursor = conn.cursor()
 # Truncate the table
-cursor.execute('TRUNCATE TABLE dbo.Instagram_Page_Statistics_Lifetime_KS')
+cursor.execute(f'TRUNCATE TABLE {db_table}')
 
-sql_query = '''INSERT INTO dbo.Instagram_Page_Statistics_Lifetime_KS (id, name, username, followers_count, follows_count, media_count, website, profile_picture_url,biography)
-               VALUES (, , , , , , , , )'''
+sql_query = f'''INSERT INTO {db_table} (id, name, username, followers_count, follows_count, media_count, website, profile_picture_url,biography)
+               VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)'''
 
 # Extract relevant data from the analytics_data
 data = (analytics_data['id'], analytics_data['name'], analytics_data['username'],
